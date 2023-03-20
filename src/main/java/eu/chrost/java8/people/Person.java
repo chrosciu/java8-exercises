b19d7118ef50f69e1ebd835fe8a98276596a9a62
Marcin Chrost <mchrost82@gmail.com>
2023-03-20 15:45:28 +0100

    used Lombok in Person

--- src/main/java/eu/chrost/java8/people/Person.java
+++ src/main/java/eu/chrost/java8/people/Person.java
@@ -1,12 +1,18 @@
 package eu.chrost.java8.people;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@EqualsAndHashCode
+@ToString
 public class Person {
-
     private final String name;
     private final Sex sex;
     private final int weight;
@@ -23,57 +29,4 @@ public class Person {
         this.phoneNumbers = new HashSet<>(Arrays.asList(phoneNumbers));
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public Sex getSex() {
-        return sex;
-    }
-
-    public int getWeight() {
-        return weight;
-    }
-
-    public Set<Phone> getPhoneNumbers() {
-        return phoneNumbers;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{name='" + name + '\'' + ", sex=" + sex + ", weight=" + weight + ", height=" + height + ", dateOfBirth=" + dateOfBirth + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Person person = (Person) o;
-        return height == person.height
-                && weight == person.weight
-                && dateOfBirth.equals(person.dateOfBirth)
-                && name.equals(person.name)
-                && phoneNumbers.equals(person.phoneNumbers)
-                && sex == person.sex;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name.hashCode();
-        result = 31 * result + sex.hashCode();
-        result = 31 * result + weight;
-        result = 31 * result + height;
-        result = 31 * result + dateOfBirth.hashCode();
-        result = 31 * result + phoneNumbers.hashCode();
-        return result;
-    }
 }
